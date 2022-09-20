ee152e3f17f79f2a61e02bc5eddf1e8505e61da3
ArkaneOnline <arkane@wservers.tk>
2022-09-20 00:43:32 -0500

    testing gcloud app engine

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ require("dotenv").config();
 
 client.on("ready", () => {
     console.log(`${client.user.tag} is online!`);
+    let server = client.guilds.cache.get("640749553313906713");
+    let channel = server.channels.cache.get("669263124570046503");
+    channel.send("Testing Google App Engine Cloud Hosting.")
 })
 
 client.login(process.env.token)
