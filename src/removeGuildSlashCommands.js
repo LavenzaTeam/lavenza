2e133d199f971060d2dc00f594d15672988b534f
ArkaneOnline <arkane@wservers.tk>
2022-09-22 05:37:19 -0500

    add a couple of useful scripts in case i make an oopsie

---
+++ src/removeGuildSlashCommands.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+
+const token = process.env.token;
+const clientId = "639988194229878810";
+const guildId = "640749553313906713";
+    
+const rest = new REST({ version: '9' }).setToken(token);
+rest.get(Routes.applicationGuildCommands(clientId, guildId))
+    .then(data => {
+        const promises = [];
+        for (const command of data) {
+            const deleteUrl = `${Routes.applicationGuildCommands(clientId, guildId)}/${command.id}`;
+            promises.push(rest.delete(deleteUrl));
+        }
+        return Promise.all(promises);
+    });
