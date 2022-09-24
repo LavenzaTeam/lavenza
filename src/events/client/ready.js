f1d53323efc5aad872a4cf99e3c52d2d5614cfa8
ArkaneOnline <arkane@wservers.tk>
2022-09-24 04:00:13 -0500

    add custom status

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -3,5 +3,6 @@ module.exports = {
     once: true,
     async execute(client) {
         console.log(`${client.user.tag} is online!`);
+        client.user.setPresence({ activities: [{ name: 'Early Dev Build! | https://lavenza.tk/' }], status: 'dnd' });
     }
 }
