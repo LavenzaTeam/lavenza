91ada943023573f2cbadd44d19830523f7c4f038
ArkaneOnline <arkane@wservers.tk>
2022-09-24 04:45:13 -0500

    add heartbeat request to status page

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -1,8 +1,16 @@
+const fetch = require("node-fetch");
+
 module.exports = {
     name: "ready",
     once: true,
     async execute(client) {
         console.log(`${client.user.tag} is online!`);
         client.user.setPresence({ activities: [{ name: 'Early Dev Build! | https://lavenza.tk/' }], status: 'dnd' });
+
+        var timer = 3, interval = timer * 60 * 1000;
+        setInterval(function() {
+	        fetch("https://betteruptime.com/api/v1/heartbeat/zZn9WwmEaNDRvtRsaEWJTDB9")
+            console.log(`${client.user.username}: Heartbeat sent.`);
+        }, interval);
     }
 }
