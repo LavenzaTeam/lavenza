6f3a6efb3d5ab587087788813cb4d26ec3db9cb5
ArkaneOnline <arkane@wservers.tk>
2022-10-05 05:29:20 -0500

    finish persona lookup of p5r command

--- src/commands/persona/p5r.js
+++ src/commands/persona/p5r.js
@@ -40,11 +40,72 @@ module.exports = {
                     components: [new ActionRowBuilder().addComponents(errorButton)]
                 });
 
+
+                //skills
+                const skillList = res[search].skills
+                var skillsMessage = "";
+
+                if (Object.keys(skillList).length > 24) {
+                    var skillMessageIII = "";
+                    var skillsMessageII = "";
+                    let num = 0;
+                    for (const skills in skillList) {
+                        let skillString = `**${skillList[skills].name} (${skillList[skills].cost})** - ${skillList[skills].effect} - Level: ${skillList[skills].level} \n`;
+                        num++
+                        if (num > 24) {
+                            skillMessageIII = skillMessageIII.concat(skillString);
+                        } else if (num >12) {
+                            skillsMessageII = skillsMessageII.concat(skillString);
+                        } else {
+                            skillsMessage = skillsMessage.concat(skillString);
+                        }
+                    }
+
+                } else if (Object.keys(skillList).length > 12) {
+                    var skillsMessageII = "";
+                    let num = 0;
+                    for (const skills in skillList) {
+                        let skillString = `**${skillList[skills].name} (${skillList[skills].cost})** - ${skillList[skills].effect} - Level: ${skillList[skills].level} \n`;
+                        num++
+                        if (num > 12) {
+                            skillsMessageII = skillsMessageII.concat(skillString);
+                        } else {
+                            skillsMessage = skillsMessage.concat(skillString);
+                        }
+                    }
+                } else {
+                    for (const skills in skillList) {
+                        let skillString = `**${skillList[skills].name} (${skillList[skills].cost})** - ${skillList[skills].effect} - Level: ${skillList[skills].level} \n`;
+                        skillsMessage = skillsMessage.concat(skillString);
+                    }
+                }
+
+                //affinities
+                let affinitiesMessage = `Physical: ${res[search].affinities.phys} | Gun: ${res[search].affinities.gun} | Fire: ${res[search].affinities.fire} | Ice: ${res[search].affinities.ice} | Elec: ${res[search].affinities.elec} | Wind: ${res[search].affinities.wind} | Psy: ${res[search].affinities.psy} | Nuke: ${res[search].affinities.nuke} | Bless: ${res[search].affinities.bless} | Curse: ${res[search].affinities.curse}`
+
                 var embed = new EmbedBuilder()
                     .setColor("Red")
                     .setTitle(res[search].name)
+                    .setThumbnail("https://lavenza.tk/assets/P5R_Logo.png")
+                    .setDescription(`**Persona Trait** - ${res[search].trait}`)
+                    .addFields(
+                        { name: "Arcana", value: res[search].arcana, inline: true },
+                        { name: "Level", value: res[search].level, inline: true },
+                        { name: "Stats", value: `Strength: ${res[search].baseStats.strength} | Magic: ${res[search].baseStats.magic} | Endurance: ${res[search].baseStats.endurance} | Agility: ${res[search].baseStats.agility} | Luck: ${res[search].baseStats.luck}`, inline: true },
+                        //{ name: '\u200b', value: '\u200b' },
+                        { name: "Affinities", value: affinitiesMessage },
+                        //{ name: '\u200b', value: '\u200b' },
+                        { name: "Item", value: res[search].item, inline: true },
+                        { name: "Alarm Item", value: res[search].itemAlarm, inline: true },
+                        { name: '\u200b', value: '\u200b' },
+                        { name: "Skills", value: skillsMessage }
+                    )
                     .setTimestamp()
                     .setFooter({ text: "Data from the Lavenza API was provided by the SMT Fandom Wiki", iconURL: client.user.displayAvatarURL() });
+
+                    if (skillsMessageII) embed.addFields( { name: '\u200b', value: skillsMessageII });
+                    if (skillMessageIII) embed.addFields( { name: '\u200b', value: skillMessageIII });
+                    embed.addFields({ name: '\u200b', value: '\u200b' });
 
                 break;
         
